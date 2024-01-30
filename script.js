e6a01b6ae5ad32d4c6e9d56fe87047bb1e09e449
Mady-T <adithya5463sajith@gmail.com>
2024-01-30 23:32:01 +0530

    Add operator, equal, AC  and numeric button functionality

---
+++ script.js
@@ -0,0 +1,71 @@
+const buttonContainer = document.querySelector('.button-panel');
+const inputField = document.querySelector('.input');
+const outputField = document.querySelector('.left-display')
+
+buttonContainer.addEventListener('click', (e) => {
+    const clickedButton = e.target;
+    if (clickedButton.classList.contains('numeric')){
+        inputField.value += clickedButton.textContent;
+    } else if (clickedButton.classList.contains('operator')) {
+        const activeOperator = document.querySelector('.active');
+        if (inputField.value !== '') {  
+            if (outputField.textContent !== '') { //perform calculation
+                if (activeOperator) {
+                    outputField.textContent = evaluate(outputField.textContent, inputField.value, activeOperator.id);
+                } else {
+                    outputField.textContent = inputField.value;
+                }
+            } else { //prepare for calculation
+                outputField.textContent = inputField.value;
+            }
+            inputField.value = '';
+        }
+        // for (const elem of document.querySelectorAll('.operator')) {
+        //     elem.classList.remove('active');
+        // }
+        if (activeOperator) {
+            activeOperator.classList.remove('active');
+        }
+        clickedButton.classList.add('active');
+    } else if (clickedButton.id === 'equal') {
+        const activeOperator = document.querySelector('.active');
+        if (inputField.value !== '') {  
+            if (outputField.textContent !== '') { //perform calculation
+                if (activeOperator) {
+                    outputField.textContent = evaluate(outputField.textContent, inputField.value, activeOperator.id);
+                }
+            } else { //prepare for calculation
+                outputField.textContent = inputField.value;
+            }
+            inputField.value = '';
+        }
+        if (activeOperator) {
+            activeOperator.classList.remove('active');
+        }
+    } else if (clickedButton.id === 'ac') {
+        outputField.textContent = '';
+        inputField.value = '';
+        for (const elem of document.querySelectorAll('.operator')) {
+            elem.classList.remove('active');
+        }
+    }
+});
+
+function evaluate(a, b, operator) {
+    a = +a;
+    b = +b;
+    switch(operator) {
+        case 'add':
+            return a + b;
+        case 'subtract':
+            return a - b;
+        case 'multiply':
+            return a * b;
+        case 'divide' :
+            return a / b;
+        case 'mod' :
+            return a % b;
+        default:
+            return operator;
+    }
+}
