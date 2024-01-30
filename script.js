ff33d0cfb941cc0356c2f9b2fe08fb75b99f608d
Mady-T <adithya5463sajith@gmail.com>
2024-01-31 00:48:57 +0530

    Complete UI redesign; Complete v1.0

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const buttonContainer = document.querySelector('.button-panel');
 const inputField = document.querySelector('.input');
-const outputField = document.querySelector('.left-display')
+const outputField = document.querySelector('.output')
 
 buttonContainer.addEventListener('click', (e) => {
     const clickedButton = e.target;
+    
     if (clickedButton.classList.contains('numeric')){
         inputField.value += clickedButton.textContent;
+
     } else if (clickedButton.classList.contains('operator')) {
         const activeOperator = document.querySelector('.active');
         if (inputField.value !== '') {  
@@ -20,13 +22,11 @@ buttonContainer.addEventListener('click', (e) => {
             }
             inputField.value = '';
         }
-        // for (const elem of document.querySelectorAll('.operator')) {
-        //     elem.classList.remove('active');
-        // }
         if (activeOperator) {
             activeOperator.classList.remove('active');
         }
         clickedButton.classList.add('active');
+
     } else if (clickedButton.id === 'equal') {
         const activeOperator = document.querySelector('.active');
         if (inputField.value !== '') {  
@@ -42,14 +42,17 @@ buttonContainer.addEventListener('click', (e) => {
         if (activeOperator) {
             activeOperator.classList.remove('active');
         }
+
     } else if (clickedButton.id === 'ac') {
         outputField.textContent = '';
         inputField.value = '';
         for (const elem of document.querySelectorAll('.operator')) {
             elem.classList.remove('active');
         }
+
     } else if (clickedButton.id === 'backspace') {
         inputField.value = inputField.value.slice(0,-1);
+        
     } else if (clickedButton.id === 'sign') {
         if (inputField.value) {
             inputField.value *= -1;
